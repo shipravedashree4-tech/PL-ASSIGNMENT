54668e7cac64cef5f41d130f6dff68bea715d92e
shipravedashree4-tech <shipravedashree4@gmail.com>
2026-03-31 11:38:46 +0530

    Add files via upload

---
+++ UNIT 3/EXP 1.py
@@ -0,0 +1,13 @@
+import math
+
+# Calculate area of circle
+def circle_area(radius):
+    return math.pi * radius * radius
+
+# Calculate area of rectangle
+def rectangle_area(length, width):
+    return length * width
+
+# Calculate area of triangle
+def triangle_area(base, height):
+    return 0.5 * base * height
